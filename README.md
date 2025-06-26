65a8d8a012a2665054bec3d19a1dc3e11f39b76e
Juergen Pabel <juergen@pabel.net>
2025-06-26 17:28:44 +0200

    Initial version

--- README.md
+++ README.md
@@ -5,22 +5,54 @@ M365 OneDrive seems to have (had?) a bug that lead to file corruption. A friend 
 Now, there are dozens of reports on the web about onedrive issues with file corruptions - what sets this issue one apart is:
 - when just going through the directories, everything seems just fine - all expected file(name)s are there
 - upon opening a(lmost any) file, the application will complain about file/data corruptions
-- my friend had escalated this to Microsoft top management in Germany and even got special assistance from experts in redmond
+- my friend had escalated this to Microsoft top management in Germany and even got special assistance from experts in Redmond
 
-This is where I came in: After Microsoft had analyzed his onedrive for a few days, they told him that they had no idea what might have happened or how to recover his files. So he called me: I took a look at some of those files using notepad and hex viewers and noticed that they appeared to be just fine - they were just mixed up in terms of file contents and file names: a word file (.docx) would have had the contents of a PDF (.pdf) - or any random combination. Heck, there were even some files that opened just fine - they just had unexpected contents, like a PDF named something-invoice.pdf contained a printer manual. All inspected files contained contents that belonged to my friend, so this isn't about a cross-tenant corruption - it's "just" file metadata and file contents that were randomly "shuffled". 
+This is where I came in: After Microsoft had analyzed his onedrive for a few days, they told him that they had no idea what might have happened or how to recover his files. So he called me: I took a look at some of those files using a text editor and noticed that they appeared to be just fine - they were just mixed up in terms of file contents and file names: a word file (.docx) would have had the contents of a PDF (.pdf) - or any random combination. Heck, there were even some files that opened just fine - they just had unexpected contents, like a PDF named `something-invoice.pdf` contained a printer manual. All inspected files contained contents that belonged to my friend, so this isn't about a cross-tenant corruption - it's "just" file metadata and file contents that were randomly "shuffled". 
 
-I already had a hunch that this might be due to either a bug in the MacOS onedrive client by itself - or some sort of concurrency issue while using multiple onedrive clients (my friend also uses Windows clients with onedrive for the same tenant+drive). My next analysis step was to take a look at some history versions of those files - but they also all contained the wrong contents. My assumption seemed to be confirmed after I looked into the "details" of some files: they all had been renamed (this information doesn't show up in the version history on onedrive): the printer-manual.pdf was renamed to something-invoice.pdf and just like that something.docx was renamed something-else.gif (or whatever). I (manually) confirmed for a few files that the filename listed in the rename operation (from the detail view) did point to the file whose contents belonged to the inspected "source" file. 
+I already had a hunch that this might be due to either a bug in the MacOS onedrive client by itself - or some sort of concurrency issue while using multiple onedrive clients (my friend also uses Windows clients with onedrive for the same tenant+drive). My next analysis step was to take a look at some history versions of those files - but they also all contained the wrong contents. My assumption seemed to be confirmed after I looked into the "details" of some files: they all had been renamed (this information doesn't show up in the version history on onedrive): the `printer-manual.pdf` was renamed to `something-invoice.pdf` and just like that `something.docx` was renamed `something-else.gif` (or whatever). I (manually) confirmed for a few files that the filename listed in the rename operation (from the detail view) did point to the file whose contents belonged to the inspected "source" file - so it seems to be due to a single "incident" (both in terms of root-cause and effects). 
 
 # Concept: Deslushing onedrive
 Apparently, we had all the needed information to recover ("deslush") his files from onedrive:
 - iterate over all files, build a (key/value) map containing the filename and where the (last) rename-operation-record points to (another filename)
 - because the filename operation lists just the pure filename but not the directory (it was almost always in a different directory), a helper map was needed to map out files with the same filename but in different directories
 - download all the file contents (onedrive objects) and rebuild the directory tree with all files and their original contents
-Now, this doesn't work out perfectly: as in almost any file, there are files with identical filenames (but in different directories). To at least aid with this, my approach is to than create a directory with the name of the currently processed file (like something-invoice.pdf) and in that directory to create a list of (numbered) files, each containing the contents of whatever file (whose filename matches) in whatever directory. Of cource, this is done using symlinks.
+Now, this doesn't work out perfectly: as in almost any file, there are files with identical filenames (but in different directories). To at least aid with this, my approach is to than create a directory with the name of the currently processed file (like `something-invoice.pdf`) and in that directory to create a list of (numbered) files, each containing the contents of whatever file (whose filename matches) in whatever directory. Of cource, this is done using symlinks.
 
-# Implementation: onedrive-deslusher.py
-First of: my implementation doesn't write to onedrive servers - it only downloads (all) file contents and reads metadata. No risk of further messing up your onedrive. But it requires downloading all files and thus consumes internet bandwith and local file storage. Make sure you have plenty of both available.
+# onedrive-deslusher.py
+
+## Application logic
+First of: my implementation doesn't write to onedrive servers - it only downloads (all) file contents and reads metadata. No risk of further messing up your onedrive. But it requires downloading all files and thus consumes internet bandwith and local file storage. Make sure you have plenty of both.
 
 This script downloads all file contents from onedrive and stores them in a dedicated directory using the onedrive object-id as the filename. It than iteraters over all files, extracts the (last) rename-operation record, checks whether this happened after a specified datetime (an when this whole mess happened) in order to not include "regular" file renames from happier days and than creates the corresponding symlinks. That should leave you with a local copy of your onedrive **before** microsoft turned on its data slushing algorithm (okay, okay, I truly believe this is a rare concurrency-related bug - but still, we are talking about software and services that cost quite a bit of money and people and organisations rely on it to work correctly).
 
-This script has been successfuly used to recover about **XX**% of all files and for all the remaining files, my friend at least has directories containing all the respective candidate files (same filenames) in order to manually finish the deslushing (I bet my whole part in this endeavour was more fun...I feel your pain, Wolfgang).
+## Setup
+
+```
+python -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+
+This runs all steps one-after-the-other (browser-interactive M365 confirmation may be required for each step):
+`./onedrive-deslusher.py --client-id xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx --user-id username@subdomain.onmicrosoft.com --datetime yyyy-mm-ddThh:mm:ssZ run`
+
+This runs all steps individually:
+`./onedrive-deslusher.py --client-id xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx --user-id username@subdomain.onmicrosoft.com get-drives`
+`./onedrive-deslusher.py --client-id xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx --user-id username@subdomain.onmicrosoft.com download-objects`
+`./onedrive-deslusher.py --client-id xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx --user-id username@subdomain.onmicrosoft.com download-activities`
+`./onedrive-deslusher.py --datetime yyyy-mm-ddThh:mm:ssZ deslush`
+
+Optionally, the following parameters can be used to modify the behaviour:
+* `--drive-name <name-of-onedrive-instance>` to select only a specific onedrive instance
+* `--directory <path-to-directory>` to use another filesystem directory for storing data (default='./data')
+
+## Results
+After completing all steps, you'll find the following sub-directories in the data storage directory (default='./data'):
+* `objects`: This is where all files from onedrive(s) are downloaded to, filenames are their onedrive internal object-ids
+* `onedrive/<name-of-onedrive-instance>-original`: This should represent your onedrive instance as it currently is ("slushed")
+* `onedrive/<name-of-onedrive-instance>-deslushed`: This contains the result of the deslushing.
+
+Please note that both in the `*-original` and the `*-deslushed` directories, the directory structure should be as-is and contain symlinks (to the object files in `objects`)) with either the original (correct) filename as the filename - or, if filename collisions occurred (very likely), there will be a directory with the given filename and its contents are symlinks to all candidate files (with the same filename).
+
---
+++ onedrive-deslusher.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+
+from os import makedirs as os_makedirs, \
+               rmdir as os_rmdir, \
+               listdir as os_listdir, \
+               symlink as os_symlink, \
+               rename as os_rename
+from os.path import exists as os_path_exists, \
+                    abspath as os_path_abspath, \
+                    isdir as os_path_isdir, \
+                    islink as os_path_islink
+from glob import iglob as glob_iglob
+from shutil import rmtree as shutil_rmtree
+from requests import get as requests_get
+from json import load as json_load, \
+                 dump as json_dump
+from re import compile as re_compile
+from asyncio import run as asyncio_run
+from argparse import ArgumentParser as argparse_ArgumentParser
+from azure.identity import InteractiveBrowserCredential as azure_identity_InteractiveBrowserCredential
+from msgraph import GraphServiceClient as msgraph_GraphServiceClient
+from msal import PublicClientApplication as msal_PublicClientApplication
+
+
+async def get_directory_files(client, drive_id: str, drive_directory_id: str) -> list:
+    filename2id = {}
+
+    response = await client.drives.by_drive_id(drive_id).items.by_drive_item_id(drive_directory_id).children.get()
+    while response is not None:
+        for child_item in response.value:
+            if child_item.folder is None:
+                filename2id[child_item.name] = child_item.id
+        if response.odata_next_link is not None:
+            response = await client.drives.by_drive_id(drive_id).items.by_drive_item_id(drive_directory_id).children.with_url(response.odata_next_link).get(request_configuration = request_configuration)
+        else:
+            response = None
+    return filename2id
+
+
+async def get_directory_tree(client, drive_id: str, drive_directory_id: str, drive_directory_path: str='') -> list:
+    dirtree2id = {}
+
+    response = await client.drives.by_drive_id(drive_id).items.by_drive_item_id(drive_directory_id).children.get()
+    while response is not None:
+        for drive_item_child in response.value:
+            if drive_item_child.folder is not None:
+                path = f'{drive_directory_path}/{drive_item_child.name}'
+                dirtree2id[path] = drive_item_child.id
+                dirtree2id.update(await get_directory_tree(client, drive_id, drive_item_child.id, path))
+        if response.odata_next_link is not None:
+            response = await client.drives.by_drive_id(drive_id).items.by_drive_item_id(drive_directory_id).children.with_url(response.odata_next_link).get(request_configuration = request_configuration)
+        else:
+            response = None
+    return dirtree2id
+
+
+
+async def get_drive_files(client, drive_id):
+    dirpath2files = {}
+
+    dirtree2id = await get_directory_tree(client, drive_id, 'root')
+    for directory_path, directory_id in dirtree2id.items():
+        dirpath2files[directory_path] = await get_directory_files(client, drive_id, directory_id)
+    return dirpath2files
+
+
+async def get_file_details(client, user_id, drive_name, filename):
+    response = await client.drives.by_drive_id(drive_id).items.by_drive_item_id(drive_item_id).children.with_url(response.odata_next_link).get(request_configuration = request_configuration)
+
+
+async def download_objects(client, user_id, drive_id, target_directory):
+    os_makedirs(f'{target_directory}/objects/', exist_ok=True)
+    dirpath2files = await get_drive_files(client, drive_id)
+    for directory_path, directory_files in dirpath2files.items():
+        print(f"  ...{len(directory_files)} files ({directory_path})")
+        for filename_name, filename_id in directory_files.items():
+            if os_path_exists(f'{target_directory}/objects/{filename_id}') is False:
+                content = await client.drives.by_drive_id(drive_id).items.by_drive_item_id(filename_id).content.get()
+                with open(f'{target_directory}/objects/{filename_id}', 'wb') as f:
+                    f.write(content)
+    return dirpath2files
+
+
+async def command_get_drives(client, user_id, drive_name, target_directory):
+    drive2id = {}
+    os_makedirs(target_directory, exist_ok=True)
+    print("Downloading list of onedrives...")
+    for drive in (await client.users.by_user_id(user_id).drives.get()).value:
+        if drive_name is None or drive_name == drive.name:
+            drive2id[drive.name] = drive.id
+    with open(f'{target_directory}/onedrive-drives.json', 'w') as f:
+        json_dump(drive2id, f)
+
+
+async def command_download_files(client, user_id, drive_name, target_directory):
+    os_makedirs(target_directory, exist_ok=True)
+    with open(f'{target_directory}/onedrive-drives.json', 'r') as f_drives:
+        for drive_name, drive_id in json_load(f_drives).items():
+            print(f"Downloading onedrive objects (drive '{drive_name}')...")
+            dirpath2files = await download_objects(client, user_id, drive_id, target_directory)
+            with open(f'{target_directory}/onedrive-files_{drive_name}.json', 'w') as f_files:
+                json_dump(dirpath2files, f_files)
+            if os_path_exists(f'{target_directory}/onedrive/{drive_name}-original') is True:
+                shutil_rmtree(f'{target_directory}/onedrive/{drive_name}-original')
+            for directory_path, directory_files in dirpath2files.items():
+                os_makedirs(f'{target_directory}/onedrive/{drive_name}-original/{directory_path}')
+                for filename_name, filename_id in directory_files.items():
+                    os_symlink(os_path_abspath(f'{target_directory}/objects/{filename_id}'), f'{target_directory}/onedrive/{drive_name}-original/{directory_path}/{filename_name}')
+
+
+async def command_download_activities(session, user_domain, drive_name, target_directory):
+    os_makedirs(target_directory, exist_ok=True)
+    with open(f'{target_directory}/onedrive-drives.json', 'r') as f_drives:
+        for drive_name, drive_id in json_load(f_drives).items():
+            files2activities = {}
+            print(f"Downloading onedrive file activities (drive '{drive_name}')...")
+            with open(f'{target_directory}/onedrive-files_{drive_name}.json', 'r') as f_files:
+                for directory_path, directory_files in json_load(f_files).items():
+                    for filename_name, filename_id in directory_files.items():
+                        filename_path = f'{directory_path}/{filename_name}'
+                        files2activities[filename_path] = []
+                        url = f'https://{user_domain}-my.sharepoint.com/_api/v2.0/drives/{drive_id}/items/{filename_id}/activities'
+                        activities = requests_get(url, headers={'Authorization': 'Bearer ' + session['access_token']}).json()
+                        for activity in activities['value']:
+                            if 'action' in activity and 'rename' in activity['action']:
+                                files2activities[filename_path].append({'datetime': activity['times']['recordedTime'], 'filename': activity['action']['rename']['oldName']})
+                with open(f'{target_directory}/onedrive-activities_{drive_name}.json', 'w') as f_activities:
+                    json_dump(files2activities, f_activities)
+
+
+async def command_deslush(target_directory, slushed_datetime):
+    dirpath2files = {}
+    files2activities = {}
+    slushed2filename = {}
+    filename2dirs = {}
+    os_makedirs(target_directory, exist_ok=True)
+    with open(f'{target_directory}/onedrive-drives.json', 'r') as f_drives:
+        for drive_name, drive_id in json_load(f_drives).items():
+            print(f"Deslushing onedrive '{drive_name}'...")
+            with open(f'{target_directory}/onedrive-files_{drive_name}.json', 'r') as f_files:
+                dirpath2files = json_load(f_files)
+            with open(f'{target_directory}/onedrive-activities_{drive_name}.json', 'r') as f_activities:
+                files2activities = json_load(f_activities)
+                for filename_path, activities in files2activities.items():
+                    for activity in activities:
+                        if activity['datetime'] >= slushed_datetime:
+                            slushed2filename[filename_path] = activity['filename']
+                            print(f"{filename_path} was slushed by onedrive ({activity['filename']})")
+            if os_path_exists(f'{target_directory}/onedrive/{drive_name}-deslushed') is True:
+                shutil_rmtree(f'{target_directory}/onedrive/{drive_name}-deslushed')
+            os_makedirs(f'{target_directory}/onedrive/{drive_name}-deslushed')
+            for directory_path, directory_files in dirpath2files.items():
+                for filename_name, filename_id in directory_files.items():
+                    if filename_name not in filename2dirs:
+                        filename2dirs[filename_name] = []
+                    filename2dirs[filename_name].append(directory_path)
+            # create symlinks to objects as per current onedrive state
+            for directory_path in dirpath2files.keys():
+                os_makedirs(f'{target_directory}/onedrive/{drive_name}-deslushed/{directory_path}', exist_ok=True)
+        processed_filenames = []
+        for directory_path, directory_files in dirpath2files.items():
+            for filename_name, filename_id in directory_files.items():
+                filename_path = f'{directory_path}/{filename_name}'
+                filename_main, filename_ext = filename_name.rsplit('.', 1)
+                if filename_path not in slushed2filename.keys():
+                    print(f"ORIGINAL: symlinking {target_directory}/onedrive/{drive_name}-deslushed/{filename_path}/{filename_id}.{filename_ext}")
+                    if os_path_exists(f'{target_directory}/onedrive/{drive_name}-deslushed/{filename_path}') is False:
+                        os_makedirs(f'{target_directory}/onedrive/{drive_name}-deslushed/{filename_path}')
+                    os_symlink(os_path_abspath(f'{target_directory}/objects/{filename_id}'), f'{target_directory}/onedrive/{drive_name}-deslushed/{filename_path}/{filename_id}.{filename_ext}', target_is_directory=False)
+                    processed_filenames.append(filename_path)
+                else:
+                    original_filename = slushed2filename[filename_path]
+                    if original_filename in filename2dirs:
+                        original_filename_main, original_filename_ext = original_filename.rsplit('.', 1)
+                        for original_directory_path in filename2dirs[original_filename]:
+                            if os_path_exists(f'{target_directory}/onedrive/{drive_name}-deslushed/{original_directory_path}/{original_filename}') is False:
+                                os_makedirs(f'{target_directory}/onedrive/{drive_name}-deslushed/{original_directory_path}/{original_filename}')
+                            print(f'DESLUSHED: symlinking {target_directory}/onedrive/{drive_name}-deslushed/{original_directory_path}/{original_filename}/{filename_id}.{original_filename_ext}')
+                            os_symlink(os_path_abspath(f'{target_directory}/objects/{filename_id}'), f'{target_directory}/onedrive/{drive_name}-deslushed/{original_directory_path}/{original_filename}/{filename_id}.{original_filename_ext}', target_is_directory=False)
+                            processed_filenames.append(f'{original_directory_path}/{original_filename}')
+        deslushed_directories = {}
+        for path in glob_iglob(f'{target_directory}/onedrive/{drive_name}-deslushed/**', recursive=True):
+            onedrive_filename = path.removeprefix(f'{target_directory}/onedrive/{drive_name}-deslushed')
+            if onedrive_filename in processed_filenames:
+                if os_path_isdir(path) is True:
+                    files = os_listdir(path)
+                    if len(files) == 1 and os_path_islink(f'{path}/{files[0]}') is True:
+                        deslushed_directories[path] = files[0]
+        for directory, filename in deslushed_directories.items():
+            os_rename(f'{directory}/{filename}', f'{directory}_{filename}.deslushed')
+            os_rmdir(directory)
+            os_rename(f'{directory}_{filename}.deslushed', directory)
+
+
+async def main():
+    parser = argparse_ArgumentParser(prog='onedrive-deslusher.py')
+    parser.add_argument('command')
+    parser.add_argument('--user-id',    required=True,    help="M365 user-id (user@domain.onmicrosoft.com)")
+    parser.add_argument('--datetime',   default=None,     help="Datetime, after which onedrive file renames should be considered for deslushing (ISO 8601 format: 'yyyy-mm-ddThh:mm:ssZ')")
+    parser.add_argument('--drive-name', default=None,     help="OneDrive drive name ('Documents' for english, 'Dokumente' for german, ...")
+    parser.add_argument('--directory',  default='./data', help="directory path for storing all data ('./data' or somewhere else)")
+    parser.add_argument('--client-id',  default=None,     help="M365 client-id/application-id (format is 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', you must register one in M365)")
+    args = parser.parse_args()
+    print(f"Using '{args.directory}' for data storage")
+    commands = [args.command]
+    if args.command == 'run':
+        if args.client_id is None or args.datetime is None:
+            print(f"Error: --client-id and --datetime are required for command 'run' (which runs all commands in order)")
+            return
+        commands = ['get-drives', 'download-objects', 'download-activities', 'deslush']
+    client = None
+    for command in commands:
+        if command in ['get-drives', 'download-objects']:
+            if client is None and args.client_id is None:
+                print(f"Error: --client-id is required for command '{command}'")
+                return
+            client = msgraph_GraphServiceClient(credentials=azure_identity_InteractiveBrowserCredential(client_id=args.client_id), scopes=['Files.Read.All'])
+        match command:
+            case 'get-drives':
+                await command_get_drives(client, args.user_id, args.drive_name, args.directory)
+            case 'download-objects':
+                await command_download_files(client, args.user_id, args.drive_name, args.directory)
+            case 'download-activities':
+                if args.client_id is None:
+                    print(f"Error: --client-id is required for command '{command}'")
+                    return
+                subdomain = re_compile(r'^[A-Za-z0-9_.]+@(?P<subdomain>[A-Za-z0-9-]+)\.onmicrosoft.com$').match(args.user_id).groupdict().get('subdomain')
+                app = msal_PublicClientApplication(args.client_id, authority='https://login.microsoftonline.com/organizations')
+                session = app.acquire_token_interactive([f'https://{subdomain}-my.sharepoint.com/.default'])
+                await command_download_activities(session, subdomain, args.drive_name, args.directory)
+            case 'deslush':
+                if args.datetime is None:
+                    print(f"Error: --datetime is required for command '{command}'")
+                    return
+                await command_deslush(args.directory, args.datetime)
+            case default:
+                parser.print_help()
+
+asyncio_run(main())
+
